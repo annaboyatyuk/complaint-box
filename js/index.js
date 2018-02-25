308f622858d9b0ee304d442ef26998ec82d52ad6
annaboyatyuk <annaboyatyuk@gmail.com>
2018-02-24 16:32:20 -0800

    mess with js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,37 @@
 
 var slider = document.getElementById('range');
 var output = document.getElementById('number');
-output.innerHTML = slider.value; 
+output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
 };
+
+
+
+
+
+
+function oneToFour() {
+  var returnOne = 'Wow, that sounds tough.';
+}
+
+function fiveToSeven() {
+  var returnFive = 'How awful!';
+}
+
+function eightToTen() {
+  var returnEight = 'Ok seriously, this is not ok.';
+}
+
+
+
+var scaleResponse = document.getElementById('scaleResponse');
+
+function createResponse(event) {
+  var scaleValue = event.target.range.value;
+
+
+}
+
+scaleResponse.addEventListener('submit', (createResponse));
