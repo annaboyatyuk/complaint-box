6d62a5b76090a03d03563c00bc2f3f7be598f0e0
annaboyatyuk <annaboyatyuk@gmail.com>
2018-02-26 18:43:20 -0800

    merge the right branch

--- js/response.js
+++ js/response.js
@@ -2,38 +2,6 @@
 
 
 
-var allpictures = [];
-
-
-function Pictures(name, filepath) {
-  this.name = name;
-  this.filepath = filepath;
-  allpictures.push(this);
-}
-
-
-new Pictures('annoy gif', '../keywordImg/annoyAnimation.gif');
-new Pictures('annoy meme', '../keywordImg/annoyMeme.jpg');
-new Pictures('annoy quote', '../keywordImg/annoyQuote.jpg');
-new Pictures('else gif', '../keywordImg/elseAnimation.gif');
-new Pictures('else meme', '../keywordImg/elseMeme.jpg');
-new Pictures('else quote', '../keywordImg/elseQuote.jpg');
-new Pictures('sick gif', '../keywordImg/sickAnimation.gif');
-new Pictures('sick meme', '../keywordImg/sickMeme.jpg');
-new Pictures('sick quote', '../keywordImg/sickQuote.jpg');
-new Pictures('traffic gif', '../keywordImg/trafficAnimation.gif');
-new Pictures('traffic meme', '../keywordImg/trafficMeme.jpg');
-new Pictures('traffic quote', '../keywordImg/trafficQuote.jpg');
-new Pictures('weather gif', '../keywordImg/weatherAnimation.gif');
-new Pictures('weather meme', '../keywordImg/weatherMeme.jpg');
-new Pictures('weather quote', '../keywordImg/weatherQuote.jpg');
-new Pictures('work gif', '../keywordImg/workAnimation.gif');
-new Pictures('work meme', '../keywordImg/workMeme.jpg');
-new Pictures('work quote', '../keywordImg/workQuote.jpg');
-
-
-
-
 //Render the complaint image by the score and the reason
 function renderComplaintImage(){
   var complaintData = localStorage.getItem('complaintlist');
@@ -58,15 +26,8 @@ function renderComplaintImage(){
   //image path variable should now have the full path name.
   console.log(imgPathName);
 
-  var responseImage = document.getElementById('responseImage');
-  var imgFile = document.createElement('img');
-
-  for(var i = 0; i < allpictures.length; i++) {
-    if(imgPathName === allpictures[i].filepath) {
-      imgFile.src = allpictures[i].filepath;
-      responseImage.appendChild(imgFile);
-    }
-  }
+  var imageEl = document.getElementById('complaintimate');
+  imageEl.src = imgPathName;
 
 }
 renderComplaintImage();
