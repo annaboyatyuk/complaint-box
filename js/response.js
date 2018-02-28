9a0f5e144832b21e173e4ce3094bbcc0a5cf3365
annaboyatyuk <annaboyatyuk@gmail.com>
2018-02-27 18:59:54 -0800

    update responseArr

--- js/response.js
+++ js/response.js
@@ -38,12 +38,11 @@ function renderComplaintImage(){
   imageEl.src = imgPathName;
 
 }
- 
-
 
 renderComplaintImage();
 
 
+var responseArr = ['I\'m sorry', 'Wow, that sounds tough.', 'What a challenge', 'Don\'t let it ruin your day.', 'Lets hope for the best', 'Tomorrow is a new day!', 'Wow! I can\'t believe it!', 'Ok seriously, this is not ok.', 'That sounds awful.', 'That is just ridicuous.', 'So annoying.'];
 
 var responseText = document.getElementById('responseText');
 var showText = document.createElement('p');
