4fb8fd753867e221de44559118eeb71439e07f16
Richard Tae <taerichard@yahoo.com>
2018-02-28 19:11:00 -0800

    reviewing

--- js/response.js
+++ js/response.js
@@ -1,15 +1,6 @@
 'use strict';
 
-var responseQuotes = [
-  'I\'m sorry', 
-  'Wow, that sounds tough.', 
-  'What a challenge', 
-  'Don\'t let it ruin your day.', 
-  'Lets hope for the best', 'Tomorrow is a new day!', 
-  'Wow! I can\'t believe it!', 
-  'Ok seriously, this is not ok.',
-  'That sounds awful.'];
-  //hey
+
 //Render the complaint image by the score and the reason
 function renderComplaintImage(){
   var complaintData = localStorage.getItem('complaintlist');
@@ -50,5 +41,4 @@ var randomIndex = responseArr[Math.floor(Math.random() * responseArr.length)];
 showText.textContent = randomIndex;
 responseText.appendChild(showText);
 console.log(showText);
-
 
